263f8e6248c195fb1d5bdc6e5252517a0a3b53ec
taungyeon <taungyeon.0103@gmail.com>
2019-07-20 19:35:32 +0900

    setup for pip

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='common',
+    version='1.0.0',
+    url='https://github.com/taungyeon/common.git',
+    license='Free',
+    author='taungyeon',
+    # author_email='hogehoge@fuga.com',
+    description='preprocessing for ML/DL',
+    # install_requires=['setuptools'],
+    # packages=["topDirectory", "topDirectory.bottomDirectory"],
+    # entry_points={
+    #     'console_scripts': [
+    #         'top = topDirectory.top:main',
+    #         'bottom = topDirectory.bottomDirectory.bottom:sub'
+    #     ]
+    # }
+)
